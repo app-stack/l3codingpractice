4f25bb97e8c6c363e64f498dc0c5006572316995
app-stack <85025246+app-stack@users.noreply.github.com>
2021-06-03 21:08:16 +0100

    Add files via upload

---
+++ Conversion Py.py
@@ -0,0 +1,7 @@
+# How many Kgs would the user like to convert ?
+num = input ("I would like to convert Kg in Lbs  ")
+#Conversion code
+a = float(num)
+#User result after conversion
+print (a*2.205)
+print ("Lbs")
